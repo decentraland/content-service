1ef44734eea1de8b66bac4dd7b821c05ec2a68fc
Amin Arria <amin.arria@lambdaclass.com>
2018-09-24 17:53:14 -0300

    Local upload of files

--- main.go
+++ main.go
@@ -1,16 +1,80 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
+
+type uploadFile struct {
+	Name string `json:"name"`
+	Cid  string `json:"cid"`
+}
+
+func saveFile(fileHeader *multipart.FileHeader) (string, error) {
+	hash := sha256.Sum256([]byte(fileHeader.Filename))
+	hashstr := hex.EncodeToString(hash[:])
+
+	dst, err := os.Create("/tmp/" + string(hashstr))
+	if err != nil {
+		return "", err
+	}
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		return "", err
+	}
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		return "", err
+	}
+
+	return hashstr, nil
+}
 
 func mappingsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(0)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	var savedFiles []uploadFile
+
+	for _, fileHeaders := range r.MultipartForm.File {
+		fileHeader := fileHeaders[0]
+
+		hash, err := saveFile(fileHeader)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		savedFiles = append(savedFiles, uploadFile{fileHeader.Filename, hash})
+	}
+
+	fmt.Printf("%+v", savedFiles)
+
+	err = json.NewEncoder(w).Encode(savedFiles)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
 }
 
 func contentsHandler(w http.ResponseWriter, r *http.Request) {
