15f199a450aa9e997701d45b8b65104786d91add
Amin Arria <amin.arria@lambdaclass.com>
2018-09-27 14:04:32 -0300

    Changed structs and added getEstate

--- blockchain.go
+++ blockchain.go
@@ -6,14 +6,25 @@ import (
 	"net/http"
 )
 
-type apiResponse struct {
+type parcelResponse struct {
+	Ok   bool    `json:"ok"`
 	Data *parcel `json:"data"`
+}
+
+type estateResponse struct {
+	Ok   bool    `json:"ok"`
+	Data *estate `json:"data"`
 }
 
 type parcel struct {
 	ID    string `json:"id"`
 	X     int    `json:"x"`
 	Y     int    `json:"y"`
+	Owner string `json:"owner"`
+}
+
+type estate struct {
+	ID    string `json:"id"`
 	Owner string `json:"owner"`
 }
 
@@ -24,7 +35,19 @@ func getParcel(x, y int) (*parcel, error) {
 		return nil, err
 	}
 
-	var data apiResponse
-	json.NewDecoder(resp.Body).Decode(&data)
-	return data.Data, nil
+	var jsonResponse parcelResponse
+	json.NewDecoder(resp.Body).Decode(&jsonResponse)
+	return jsonResponse.Data, nil
+}
+
+func getEstate(id string) (*estate, error) {
+	url := fmt.Sprintf("https://api.decentraland.org/v1/estate/%s", id)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonResponse estateResponse
+	json.NewDecoder(resp.Body).Decode(&jsonResponse)
+	return jsonResponse.Data, nil
 }
