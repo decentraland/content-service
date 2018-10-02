e40dc14494ebdb216349fa5af97cd948fb476f53
Amin Arria <amin.arria@lambdaclass.com>
2018-10-02 12:57:19 -0300

    Updated demo

--- demo.sh
+++ demo.sh
@@ -21,6 +21,9 @@ echo '{
 
 echo 'This is a text file' > demo/assets/text.txt
 
-curl 'http://localhost:8000/mappings' -F 'metadata={"value":"/ipfs/iwjfoaoiaeefwe", "pubkey":"0x959e104e1a4db6317fa58f8295f586e1a978c297"}' -F 'koidaoidanf=@demo/scene.json' -F 'assets/iodfiofd=@demo/assets/text.txt'
+curl 'http://localhost:8000/mappings' \
+ -F 'metadata={"value":"/ipfs/iwjfoaoiaeefwe", "pubkey":"0x959e104e1a4db6317fa58f8295f586e1a978c297"}' \
+ -F 'zb2rhkshrs1L7X2LWfdFQuEZv6w9HDpWUgGagLGbVhd8NwbeJ=@demo/scene.json' \
+ -F 'assets/zb2rhhvg9yGb4fy8qF5K26Pvbx4H4xyo9SDEv9ag4286NzQX7=@demo/assets/text.txt'
 
-curl 'http://localhost:8000/contents/text.txt'
+curl 'http://localhost:8000/contents/zb2rhhvg9yGb4fy8qF5K26Pvbx4H4xyo9SDEv9ag4286NzQX7'
