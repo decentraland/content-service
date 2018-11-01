1592a3c2340c05077aedae48c662d71f524fceab
Alejandro Drabenche <alejandro@decentraland.org>
2018-11-01 16:58:44 -0300

    Preparing stg environment

--- .terraform/main/terraform-run.sh
+++ .terraform/main/terraform-run.sh
@@ -28,18 +28,16 @@ if test $? -ne 0; then
   exit 2;
 fi
 
-terraform destroy -auto-approve \
--var-file=../config/$REGION/${ENV}/default.backend \
--var-file=../config/$REGION/${ENV}/default.tfvars
-  if test $? -ne 0; then
-    echo "Unable ter perform terraform destroy"
-    exit 2;
-fi
-
-
-
 case $BRANCH in
   master|development)
+            terraform destroy -auto-approve \
+            -var-file=../config/$REGION/${ENV}/default.backend \
+            -var-file=../config/$REGION/${ENV}/default.tfvars
+              if test $? -ne 0; then
+                echo "Unable ter perform terraform destroy"
+                exit 2;
+            fi
+
             terraform apply -auto-approve \
             -var-file=../config/$REGION/${ENV}/default.backend \
             -var-file=../config/$REGION/${ENV}/default.tfvars
