51337c22084deed5f04ec90c0e377b324dded83b
Mario S <mario@decentraland.org>
2018-11-02 14:29:02 -0300

    Overwrite configs with env variables

--- config/config.go
+++ config/config.go
@@ -48,6 +48,18 @@ func GetConfig(name string) *Configuration {
 		log.Fatalf("Error reading config file, %s", err)
 	}
 
+	// Read configurations from ENV to overwrite config file values
+	// S3 Configuration
+	viper.BindEnv("s3storage.bucket", "AWS_S3_BUCKET")
+	viper.BindEnv("s3storage.url", "AWS_S3_URL")
+	viper.BindEnv("s3storage.acl", "AWS_S3_ACL")
+	// Redis Configuration
+	viper.BindEnv("redis.address", "REDIS_ADDRESS")
+	viper.BindEnv("redis.password", "REDIS_PASSWORD")
+	viper.BindEnv("redis.db", "REDIS_DB")
+	// DCL API
+	viper.BindEnv("decentralandapi.landurl", "DCL_API")
+
 	err = viper.Unmarshal(&config)
 	if err != nil {
 		log.Fatalf("Unable to decode config file into struct, %s", err)
