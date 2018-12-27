73ab920fcda1e436bdc46413c0d09aa08f82f0a3
Alejandro Drabenche <alejandro@decentraland.org>
2018-12-27 10:49:17 -0300

    Removed build lines and Jenkinsfile

--- content-service-run.sh
+++ content-service-run.sh
@@ -2,10 +2,6 @@
 export  GOPATH=/go
 export  GOCACHE=/root/.cache/go-buil
 echo " --------------------- "
-echo "| Building....        |"
-echo " --------------------- "
-go build
-echo " --------------------- "
 echo "| Running....         |"
 echo " --------------------- "
 ./content-service
