a1f61d8a008be6bdb4a7181c627162ac42d9254d
Alejandro Drabenche <alejandro@decentraland.org>
2018-12-27 10:58:06 -0300

    disabling init script

--- content-service-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export  GOPATH=/go
-export  GOCACHE=/root/.cache/go-buil
-echo " --------------------- "
-echo "| Running....         |"
-echo " --------------------- "
-./content-service
