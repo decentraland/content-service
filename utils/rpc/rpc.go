0ca02cadfa446ca226c147a7da7a05a30836b191
Matias Marquez <donequis@linux.com>
2019-07-19 19:36:55 -0300

    fix build

--- utils/rpc/rpc.go
+++ utils/rpc/rpc.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"strings"
 )
