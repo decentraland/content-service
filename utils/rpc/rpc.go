152437af9f5a31a28849a1d8a512deface065c42
Matias Marquez <donequis@linux.com>
2019-07-19 11:01:45 -0300

    basic dapper validation function

---
+++ utils/rpc/rpc.go
@@ -0,0 +1,49 @@
+package rpc
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"strings"
+)
+
+func ValidateDapperSignature(address, msg, signature string) (bool, error) {
+
+	client, _ := ethclient.Dial("https://mainnet.infura.io/v3/0720b4fd81a94f9db49ddd00257e1b59")
+	defer client.Close()
+
+	a, err := abi.JSON(strings.NewReader(methodjson))
+
+	Signature := signature
+	Value := msg
+	Address := common.HexToAddress(address)
+	Hash := [32]byte{}
+	copy(Hash[:], common.FromHex(Value)[0:32])
+	packed, err := a.Pack("isValidSignature", Hash, common.FromHex(Signature))
+	if err != nil {
+		return false, err
+	}
+	callMsg := ethereum.CallMsg{
+		To:   &Address,
+		Data: packed,
+	}
+
+	fmt.Println(common.ToHex(callMsg.Data))
+	res, err := client.CallContract(context.Background(), callMsg, nil)
+	if err != nil {
+		return false, err
+	}
+
+	magic := res[0:4]
+
+	cmp := bytes.Compare(magic, common.FromHex("0x1626ba7e"))
+	if cmp == 0 {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
